df047233304ee80def34428067bc69c811a833e0
zongshuo <onemysoft@163.com>
2018-08-20 14:15:18 +0800

    update controller

--- src/main/java/com/onemysoft/scriptflow/web/controller/RunController.java
+++ src/main/java/com/onemysoft/scriptflow/web/controller/RunController.java
@@ -1,24 +1,28 @@
 package com.onemysoft.scriptflow.web.controller;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.onemysoft.scriptflow.core.enums.ScriptSourceTypeEnum;
 import com.onemysoft.scriptflow.flow.Flow;
+import com.onemysoft.scriptflow.web.model.FlowModel;
 
 @RestController
-public class HelloController {
-
-	@RequestMapping(value = "/hello/{name}", method = RequestMethod.GET)
-    public String hello(@PathVariable("name") String name) {
+public class RunController {
+    @RequestMapping(value = "/run", method = RequestMethod.POST)
+    public void runAsText(@RequestBody FlowModel flow) {
+        System.out.println(flow.getBody());
+    }
+	@RequestMapping(value = "/run/{name}", method = RequestMethod.GET)
+    public String runAsFile(@PathVariable("name") String name) {
 		
 		
 		String returnString="";
